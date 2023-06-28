4223648016bff51ccf9601a1bafea04c5da235a4
CesarJZO <cesarjzo@outlook.com>
2023-06-27 23:06:47 -0600

    Adding multiple components using React.Fragment

--- projects/hello-world/src/main.tsx
+++ projects/hello-world/src/main.tsx
@@ -2,4 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render("Hello World!")
+root.render(
+  <React.Fragment>
+    <button>Hello Button 1</button>
+    <button>Hello Button 2</button>
+  </React.Fragment>
+)
