1d687eae44d685559da76cdde525f5d928951b0b
César Zárate <cesarjzo@outlook.com>
2023-08-02 00:29:04 -0600

    Use map on App.tsx and update gitignore

--- projects/hello-world/src/App.tsx
+++ projects/hello-world/src/App.tsx
@@ -1,19 +1,49 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
-export function App() {
-
-  const components = []
-
-  for (let i = 0; i < 5; i++) {
-    components.push(<TwitterFollowCard username={`user${i}`}>
-      César Zárate
-    </TwitterFollowCard>)
+const users = [
+  {
+    name: 'Dan Abramov',
+    username: 'dan_abramov',
+    isFollowing: true
+  },
+  {
+    name: 'Ryan Florence',
+    username: 'ryanflorence',
+    isFollowing: false
+  },
+  {
+    name: 'Michael Jackson',
+    username: 'mjackson',
+    isFollowing: true
+  },
+  {
+    name: 'Kent C. Dodds',
+    username: 'kentcdodds',
+    isFollowing: false
+  },
+  {
+    name: 'Emma Wedekind',
+    username: 'EmmaWedekind'
   }
+]
 
+export function App() {
   return (
     <section className='App'>
-      {components}
+      {
+        users.map(({ name, username, isFollowing }) => { // Destructuring the user object
+          // const { name, username, isFollowing } = user
+          return (
+            <TwitterFollowCard
+              key={username} // React needs a key to keep track of the elements in the list
+              username={username}
+              initialIsFollowing={isFollowing}>
+              <strong>{name}</strong>
+            </TwitterFollowCard>
+          )
+        })
+      }
     </section>
   )
 }
