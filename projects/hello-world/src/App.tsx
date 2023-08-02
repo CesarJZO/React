b4d7391c5f91bd03f8afc200eed2c83abf16fcd8
César Zárate <cesarjzo@outlook.com>
2023-08-01 19:20:43 -0600

    Create twitter card without css

--- projects/hello-world/src/App.tsx
+++ projects/hello-world/src/App.tsx
@@ -1,7 +1,19 @@
 export function App() {
     return (
-        <div>
-            <h1>Twitter Card</h1>
-        </div>
+        <article>
+            <header>
+                <img src="https://pbs.twimg.com/profile_images/1409692629399265283/xecXBcBR_400x400.jpg" alt="CesarJZO profile picture" />
+                <div>
+                    <strong>César Zárate</strong>
+                    <span>@CesarJZO</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Follow
+                </button>
+            </aside>
+        </article>
     )
 }
