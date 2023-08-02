833f42d8fa52910b7fdc6d1460720af97d1c0e52
César Zárate <cesarjzo@outlook.com>
2023-08-01 19:37:07 -0600

    Add styles and twitter card body in App.tsx

--- projects/hello-world/src/App.tsx
+++ projects/hello-world/src/App.tsx
@@ -1,19 +1,27 @@
-export function App() {
-    return (
-        <article>
-            <header>
-                <img src="https://pbs.twimg.com/profile_images/1409692629399265283/xecXBcBR_400x400.jpg" alt="CesarJZO profile picture" />
-                <div>
-                    <strong>César Zárate</strong>
-                    <span>@CesarJZO</span>
-                </div>
-            </header>
+import './App.css'
 
-            <aside>
-                <button>
-                    Follow
-                </button>
-            </aside>
-        </article>
-    )
+export function App() {
+  return (
+    // Declaring a class in JSX using a prefix
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src="https://pbs.twimg.com/profile_images/1409692629399265283/xecXBcBR_400x400.jpg"
+          alt="CesarJZO profile picture" />
+        <div className="tw-followCard-info">
+          <strong>César Zárate</strong>
+          <span className="tw-followCard-infoUsername">
+            @CesarJZO
+          </span>
+        </div>
+      </header>
+
+      <aside>
+        <button className="tw-followCard-button">
+          Follow
+        </button>
+      </aside>
+    </article>
+  )
 }
