aa65c70f9a02d63b3e80a036fe40040bf66be84a
César Zárate <cesarjzo@outlook.com>
2023-08-02 23:34:47 -0600

    Add onClick functions

--- projects/tic-tac-toe/src/App.tsx
+++ projects/tic-tac-toe/src/App.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const TURNS = {
-  X: 'x',
-  O: 'o'
+  X: '×',
+  O: '○'
 }
 
-const board = Array(9).fill(null)
+const Square = (props: {
+  children: React.ReactNode,
+  isSelected: boolean,
+  updateBoard: () => void,
+  index: number
+}) => {
 
-const Square = (props: { children: React.ReactNode, updateBoard, index }) => {
+  const className = `square ${props.isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    props.updateBoard()
+  }
+
   return (
-    <div className='square'>
+    <div
+      className={className}
+      onClick={handleClick}>
       {props.children}
     </div>
   )
 }
 
 function App() {
+  const [board, setBoard] = useState(
+    // ['×', '○', '×', '○', '×', '○', '×', '○', '×']
+    Array(9).fill(null)
+  )
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className='board'>
       <h1>Tic tac toe</h1>
@@ -26,12 +50,22 @@ function App() {
             return (
               <Square
                 key={index}
-                index={index}>
-                  {index}
+                index={index}
+                updateBoard={updateBoard}>
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
