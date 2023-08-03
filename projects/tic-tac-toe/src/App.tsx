4ce7a2097414ec0c7cc9bc554d6a95046d1a2ac2
César Zárate <cesarjzo@outlook.com>
2023-08-02 23:16:31 -0600

    Add Square component to App.tsx

--- projects/tic-tac-toe/src/App.tsx
+++ projects/tic-tac-toe/src/App.tsx
@@ -1,8 +1,39 @@
+import React from 'react'
 import './App.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null)
+
+const Square = (props: { children: React.ReactNode, updateBoard, index }) => {
+  return (
+    <div className='square'>
+      {props.children}
+    </div>
+  )
+}
 
 function App() {
   return (
-    <h1>Tic tac toe</h1>
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}>
+                  {index}
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
